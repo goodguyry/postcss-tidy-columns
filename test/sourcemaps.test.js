210068a988dd380e23318df0d79f6abc2cc08fed
Ryan Domingue <ryan@alley.co>
2018-09-07 20:50:08 -0700

    Adds source-map tests

--- test/sourcemaps.test.js
+++ test/sourcemaps.test.js
@@ -3,6 +3,7 @@ const tidyColumns = require('../');
 const fs = require('fs');
 const path = require('path');
 const json = require('./fixtures/_fixtures.json');
+const mapFile = require('./fixtures/source-maps/mappings.json');
 
 /**
  * Test fixtures
@@ -12,8 +13,8 @@ describe('Test CSS fixtures', () => {
   json['source-maps'].forEach((item) => {
     if (!item.skip) {
       test(`${item.description}`, () => {
-        const from = path.join(__dirname, `${item.root}/${item.name}.css`);
-        const to = path.join(__dirname, `${item.root}/${item.name}.generated.css`);
+        const from = path.join(__dirname, `${item.fixtures.input}`);
+        const to = path.join(__dirname, `${item.fixtures.expected}`);
 
         const input = fs.readFileSync(from, 'utf8');
 
@@ -27,7 +28,8 @@ describe('Test CSS fixtures', () => {
         ])
           .process(input, { from, to, map: { inline: false } })
           .then((result) => {
-            expect(result.map._mappings).toEqual({ "_array": [{ "originalLine": 1, "originalColumn": 0, "generatedLine": 1, "generatedColumn": 0, "name": null, "source": "offset-left.css" }, { "originalLine": 2, "originalColumn": 1, "generatedLine": 2, "generatedColumn": 1, "name": null, "source": "offset-left.css" }, { "originalLine": 2, "originalColumn": 21, "generatedLine": 2, "generatedColumn": 72, "name": null, "source": "offset-left.css" }, { "originalLine": 3, "originalColumn": 1, "generatedLine": 3, "generatedColumn": 1, "name": null, "source": "offset-left.css" }, { "originalLine": 2, "originalColumn": 1, "generatedLine": 4, "generatedColumn": 0, "name": null, "source": "offset-left.css" }, { "originalLine": 2, "originalColumn": 1, "generatedLine": 5, "generatedColumn": 1, "name": null, "source": "offset-left.css" }, { "originalLine": 2, "originalColumn": 1, "generatedLine": 6, "generatedColumn": 2, "name": null, "source": "offset-left.css" }, { "originalLine": 2, "originalColumn": 21, "generatedLine": 6, "generatedColumn": 73, "name": null, "source": "offset-left.css" }, { "originalLine": 2, "originalColumn": 21, "generatedLine": 7, "generatedColumn": 2, "name": null, "source": "offset-left.css" }, { "originalLine": 1, "originalColumn": 0, "generatedLine": 8, "generatedColumn": 1, "name": null, "source": "offset-left.css" }], "_last": { "originalLine": 1, "originalColumn": 0, "generatedLine": 8, "generatedColumn": 1, "name": null, "source": "offset-left.css" }, "_sorted": true }); // eslint-disable-line
+            // eslint-disable-next-line no-underscore-dangle
+            expect(result.map._mappings).toEqual(mapFile[item.mapKey]);
             expect(result.warnings().length).toBe(0);
           });
       });
