c3b3cf47dbf3059993b0ddf5934f526476561406
Ryan Domingue <rychdom+github@gmail.com>
2018-10-20 01:23:40 -0500

    Updates breakpoint matching based on latest options setup

--- test/poc-media-query-check.test.js
+++ test/poc-media-query-check.test.js
@@ -16,51 +16,50 @@ describe('Find a breakpoint match: px', () => {
         gap: '1.25rem',
       },
     ],
+    collectedBreakpointValues: [
+      '768px',
+      '1024px',
+      '1440px',
+    ],
   };
 
-  const breakpoints = [
-    '768px',
-    '1024px',
-    '1440px',
-  ];
-
   test('Between breakpoint configs (min)', () => {
-    expect(breakpointMatch('(min-width: 900px)', options, breakpoints))
+    expect(breakpointMatch('(min-width: 900px)', options))
       .toEqual({ breakpoint: '768px', gap: '0.625rem' });
   });
 
   test('Between breakpoint configs (max)', () => {
-    expect(breakpointMatch('(max-width: 900px)', options, breakpoints))
+    expect(breakpointMatch('(max-width: 900px)', options))
       .toEqual({ breakpoint: '768px', gap: '0.625rem' });
   });
 
   test('Below lowest breakpoint config (min)', () => {
-    expect(breakpointMatch('(min-width: 600px)', options, breakpoints))
+    expect(breakpointMatch('(min-width: 600px)', options))
       .toEqual(undefined);
   });
 
   test('Below lowest breakpoint config (max)', () => {
-    expect(breakpointMatch('(max-width: 600px)', options, breakpoints))
+    expect(breakpointMatch('(max-width: 600px)', options))
       .toEqual(undefined);
   });
 
   test('Up-to breakpoint config', () => {
-    expect(breakpointMatch('(max-width: 1439px)', options, breakpoints))
+    expect(breakpointMatch('(max-width: 1439px)', options))
       .toEqual({ breakpoint: '1024px', gap: '1rem' });
   });
 
   test('Up-to lowest breakpoint config', () => {
-    expect(breakpointMatch('(max-width: 767px)', options, breakpoints))
+    expect(breakpointMatch('(max-width: 767px)', options))
       .toEqual(undefined);
   });
 
   test('Between breakpoint configs (min-max)', () => {
-    expect(breakpointMatch('(min-width: 768px) and (max-width: 1023px)', options, breakpoints))
+    expect(breakpointMatch('(min-width: 768px) and (max-width: 1023px)', options))
       .toEqual({ breakpoint: '768px', gap: '0.625rem' });
   });
 
   test('Spanning two breakpoint configs (min-max)', () => {
-    expect(breakpointMatch('(min-width: 768px) and (max-width: 1439px)', options, breakpoints))
+    expect(breakpointMatch('(min-width: 768px) and (max-width: 1439px)', options))
       .toEqual(undefined);
   });
 });
