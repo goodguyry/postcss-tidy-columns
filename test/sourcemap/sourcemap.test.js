8678690474701a385e727115f9112433fcc96b2b
Ryan Domingue <rychdom+github@gmail.com>
2019-03-31 11:42:10 -0700

    Cleans up sourcemap tests

--- test/sourcemap/sourcemap.test.js
+++ test/sourcemap/sourcemap.test.js
@@ -2,31 +2,31 @@ const postcss = require('postcss');
 const tidyColumns = require('../../');
 const fs = require('fs');
 const sourceMapTests = require('./sourcemap.js');
-const { typical } = require('../sharedConfigs');
 
 /**
  * Test sourcemaps
  * Reads JSON file of test declarations.
  */
 describe('Sourcemaps are maintained after plugin processing', () => {
-  sourceMapTests.forEach((item) => {
-    if (!item.skip) {
-      test(`${item.description}`, () => {
-        const { from, to } = item.fixtures;
-        const input = fs.readFileSync(from, 'utf8');
+  const reducedTests = sourceMapTests.reduce(
+    (acc, test) => [...acc, Object.values(test)],
+    [],
+  );
 
-        return postcss([
-          tidyColumns(Object.assign(typical, item.options)),
-        ])
-          .process(input, { from, to, map: { inline: false } })
-          .then((result) => {
-            expect(JSON.parse(result.map)).toEqual(item.map);
-            expect(result.warnings().length).toBe(0);
-          });
-      });
-    } else {
-      // Return `null` for skipped tests.
-      test.skip(`${item.description}`, () => null);
-    }
-  });
+  test.each(reducedTests)(
+    '%s',
+    (description, options, map, fixtures) => {
+      const { from, to } = fixtures;
+      const input = fs.readFileSync(from, 'utf8');
+
+      return postcss([
+        tidyColumns(options),
+      ])
+        .process(input, { from, to, map: { inline: false } })
+        .then((result) => {
+          expect(JSON.parse(result.map)).toEqual(map);
+          expect(result.warnings().length).toBe(0);
+        });
+    },
+  );
 });
