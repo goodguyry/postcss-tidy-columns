9d6dd7ab66a5b6d3a6afdf3ecc8655050afa3784
Ryan Domingue <rychdom+github@gmail.com>
2019-06-02 00:50:00 -0700

    Adds autoprefixer and corrects output path

--- docs/client/build.js
+++ docs/client/build.js
@@ -1,22 +1,24 @@
-const postcss = require('postcss');
 const fs = require('fs');
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
 const tidyColumns = require('../../');
 
 fs.readFile('docs/client/styles.in.css', (err, css) => {
   postcss([
     tidyColumns({
       columns: 12,
-      siteMax: '90rem',
+      siteMax: '80rem',
       gap: '1.25rem',
-      edge: '1.5rem',
+      edge: '1.875rem',
       debug: true,
     }),
+    autoprefixer,
   ])
-    .process(css, { from: 'docs/client/styles.in.css', to: 'docs/dist/styles.out.css' })
+    .process(css, { from: 'docs/client/styles.in.css', to: 'docs/client/styles.out.css' })
     .then((result) => {
-      fs.writeFile('docs/dist/styles.out.css', result.css, () => true);
+      fs.writeFile('docs/client/styles.out.css', result.css, () => true);
       if (result.map) {
-        fs.writeFile('docs/dist/styles.out.css.map', result.map, () => true);
+        fs.writeFile('docs/client/styles.out.css.map', result.map, () => true);
       }
     });
 });
