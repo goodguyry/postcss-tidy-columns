89036edf1acf58158488fe71872d0cfbd1d9ea0d
Ryan Domingue <rychdom+github@gmail.com>
2019-09-20 23:55:08 -0700

    Incorporates tidy.atRules

--- tidy-propagation.js
+++ tidy-propagation.js
@@ -1,46 +1,44 @@
-const postcss = require('postcss');
 const cleanClone = require('./lib/cleanClone');
+const getObjectByProperty = require('./lib/getObjectByProperty');
 
 function tidyPropagation(declaration, tidy, root) {
-  const { columns: { options: { breakpoints } } } = tidy;
+  const { atRules, columns: { options: { breakpoints } } } = tidy;
   const rule = declaration.parent;
 
   // Reverse the breakpoints to make sure they're inserted in the correct order.
   Object.keys(breakpoints).reverse().forEach((breakpoint) => {
-    // Clone the declaration without `!tidy`.
-    const cleanDecl = cleanClone(
+    const atRule = getObjectByProperty(atRules, `(min-width: ${breakpoint})`, 'params');
+
+    if (undefined !== atRule) {
+      // Clone the declaration without `!tidy`.
+      const cleanDecl = cleanClone(
+        declaration,
+        {
+          declaration: declaration.prop,
+          value: declaration.value.replace(/\s?!tidy/, ''),
+        },
+      );
+
+      // Clone the rule and add the cloned declaration.
+      const newRule = cleanClone(rule);
+      newRule.append(cleanDecl);
+
+      // @todo Need to ensure declarations within atrules are applied as expected
+
+      atRule.append(newRule);
+
+      root.insertAfter(rule, atRule);
+    }
+
+    // Replace the declaration with `!tidy` clipped off.
+    declaration.replaceWith(cleanClone(
       declaration,
       {
         declaration: declaration.prop,
         value: declaration.value.replace(/\s?!tidy/, ''),
       },
-    );
-
-    // Clone the rule and add the cloned declaration.
-    const newRule = cleanClone(rule);
-    newRule.append(cleanDecl);
-
-    // @todo Need to ensure declarations within atrules are applied as expected
-
-    // Create the atRule and append the rule.
-    const breakpointAtRule = postcss.atRule({
-      name: 'media',
-      params: `(min-width: ${breakpoint})`,
-      nodes: [],
-      source: rule.source,
-    }).append(newRule);
-
-    root.insertAfter(rule, breakpointAtRule);
+    ));
   });
-
-  // Replace the declaration with `!tidy` clipped off.
-  declaration.replaceWith(cleanClone(
-    declaration,
-    {
-      declaration: declaration.prop,
-      value: declaration.value.replace(/\s?!tidy/, ''),
-    },
-  ));
 }
 
 module.exports = {
