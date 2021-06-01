b947d831ec7e2c14407441ecaedad87d01547443
Ryan Domingue <rychdom+github@gmail.com>
2021-05-31 17:20:01 -0700

    Uses PostCSS 8 events

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const postcss = require('postcss');
 const Tidy = require('./Tidy');
 const getGlobalOptions = require('./src/getGlobalOptions');
 const { tidyShorthandProperty } = require('./tidy-shorthand-property');
@@ -13,21 +12,32 @@ const { tidyVar } = require('./tidy-var');
  */
 module.exports = (options = {}) => ({
   postcssPlugin: 'postcss-tidy-columns',
-  Once(root) {
-    // Collect the global options.
-    const globalOptions = Object.freeze(getGlobalOptions(root, options));
+  prepare() {
+    let globalOptions = {};
+    let tidy = {};
 
-    // Parse rules and declarations, replace `tidy-` properties.
-    root.walkRules((rule) => {
-      const tidy = new Tidy(rule, globalOptions);
+    return {
+      /**
+       * Collect the global options.
+       */
+      Once(root) {
+        globalOptions = Object.freeze(getGlobalOptions(root, options));
+      },
 
-      // Set up rule-specific properties.
-      tidy.initRule();
+      /**
+       * Set up rule-specific properties.
+       */
+      Rule(rule) {
+        tidy = new Tidy(rule, globalOptions);
 
-      /*
+        // Set up rule-specific properties.
+        tidy.initRule();
+      },
+
+      /**
        * Replace shorthand properties and option value references first
        */
-      rule.walkDecls((declaration) => {
+      Declaration(declaration) {
         // Replace shorthand declarations with their long-form equivalents.
         if (
           declaration.prop.includes('tidy-column')
@@ -40,55 +50,58 @@ module.exports = (options = {}) => ({
         if (declaration.value.includes('tidy-var')) {
           tidyVar(declaration, tidy);
         }
-      });
+      },
 
-      /*
+      /**
        * Replace property and function values with columns expressions.
        */
-      rule.walkDecls((declaration) => {
-        // Replace `tidy-*` properties.
-        if (
-          declaration.prop.includes('tidy-span')
-          || declaration.prop.includes('tidy-offset')
-        ) {
-          tidyProperty(declaration, tidy);
+      RuleExit(rule, { result, atRule }) {
+        rule.walkDecls((declaration) => {
+          // Replace `tidy-*` properties.
+          if (
+            declaration.prop.includes('tidy-span')
+            || declaration.prop.includes('tidy-offset')
+          ) {
+            tidyProperty(declaration, tidy);
+          }
+
+          // Replace `tidy-[span|offset]()` and `tidy-[span|offset]-full()` functions.
+          if (
+            declaration.value.includes('tidy-span')
+            || declaration.value.includes('tidy-offset')
+          ) {
+            tidyFunction(declaration, tidy);
+          }
+        });
+
+        const { fullWidthRule } = tidy;
+        const { siteMax } = tidy.columns.options;
+        const { root } = result;
+
+        // Add the media query if a siteMax is declared and the `fullWidthRule` has children.
+        if (undefined !== siteMax && fullWidthRule.nodes.length > 0) {
+          /**
+           * The siteMax-width atRule.
+           * Contains full-width margin offset declarations.
+           */
+          const fullWidthAtRule = atRule({
+            name: 'media',
+            params: `(min-width: ${siteMax})`,
+            nodes: [],
+            source: rule.source,
+          }).append(fullWidthRule);
+
+          // Insert the media query
+          if ('atrule' === rule.parent.type) {
+            // Insert after the parent at-rule.
+            root.insertAfter(rule.parent, fullWidthAtRule);
+          } else {
+            // Insert after the current rule.
+            root.insertAfter(rule, fullWidthAtRule);
+          }
         }
-
-        // Replace `tidy-[span|offset]()` and `tidy-[span|offset]-full()` functions.
-        if (
-          declaration.value.includes('tidy-span')
-          || declaration.value.includes('tidy-offset')
-        ) {
-          tidyFunction(declaration, tidy);
-        }
-      });
-
-      const { fullWidthRule } = tidy;
-      const { siteMax } = tidy.columns.options;
-
-      // Add the media query if a siteMax is declared and the `fullWidthRule` has children.
-      if (undefined !== siteMax && fullWidthRule.nodes.length > 0) {
-        /**
-         * The siteMax-width atRule.
-         * Contains full-width margin offset declarations.
-         */
-        const fullWidthAtRule = postcss.atRule({
-          name: 'media',
-          params: `(min-width: ${siteMax})`,
-          nodes: [],
-          source: rule.source,
-        }).append(fullWidthRule);
-
-        // Insert the media query
-        if ('atrule' === rule.parent.type) {
-          // Insert after the parent at-rule.
-          root.insertAfter(rule.parent, fullWidthAtRule);
-        } else {
-          // Insert after the current rule.
-          root.insertAfter(rule, fullWidthAtRule);
-        }
-      }
-    });
+      },
+    };
   },
 });
 
